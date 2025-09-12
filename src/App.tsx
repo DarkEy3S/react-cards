96e33908227c1aa2d5acd99b0fb2944fa4bf8bb0
Sevak PC <sevak.martirosyan1@gmail.com>
2025-09-13 09:10:17 +1000

    Home page, question card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { MainLayout } from "./components/MainLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div>forbidden!!!</div>} />
           <Route path="/addquestion" element={<div>add question</div>} />
         </Route>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,21 @@
 import {} from "react";
-import logoReact from "/public/vite.svg";
+import logoReact from "/public/react.svg";
 import cls from "./Header.module.css";
 import { Button } from "../Button";
+import { useNavigate } from "react-router-dom";
 
 export const Header: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <header className={cls.header}>
-      <p>
+      <p onClick={() => navigate("/")}>
         <img src={logoReact} alt="react logo" />
         <span>ReactCards</span>
       </p>
 
       <div className={cls.headerButtons}>
-        <Button>Add</Button>
+        <Button onClick={() => navigate("/addquestion")}>Add</Button>
         <Button>Login</Button>
       </div>
     </header>
