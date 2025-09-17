de23cceb79473ac643741840d73dd5768b646552
Sevak PC <sevak.martirosyan1@gmail.com>
2025-09-17 13:40:58 +1000

    lazy edit question page

--- src/pages/EditQuestionPage/EditQuestionPage.tsx
+++ src/pages/EditQuestionPage/EditQuestionPage.tsx
@@ -5,7 +5,7 @@ import { Loader } from "../../components/Loader";
 import { API_URL } from "../../constants";
 import { EditQuestion } from "./EditQuestion.tsx";
 
-export const EditQuestionPage = () => {
+const EditQuestionPage = () => {
   const { id } = useParams();
   const [question, setQuestion] = useState(null);
 
@@ -27,3 +27,5 @@ export const EditQuestionPage = () => {
     </div>
   );
 };
+
+export default EditQuestionPage;
