d4fc12e8428a29c0898c00dcbc825c9b2c928b5b
Sevak PC <sevak.martirosyan1@gmail.com>
2025-09-18 12:54:57 +1000

    added .env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8806";
+export const API_URL = import.meta.env.VITE_API_URL;
 export const AUTH_STORAGE = "reactCardLogin";
 export const THEME_STORAGE = "reactCardTheme";
